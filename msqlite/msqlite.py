2be325a648b22db7d90f08b9e874e19b06eb1654
JamesAbel <j@abel.co>
2024-03-10 11:46:37 -0700

    dev continues

--- msqlite/msqlite.py
+++ msqlite/msqlite.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 log = getLogger(__name__)
 
-MAX_TRIES = 10000
+MAX_TRIES = 1000
 MAX_BACKOFF = 0.1  # seconds
 
 
@@ -20,31 +20,54 @@ class MSQLite:
     """
 
     def __init__(self, db_path: Path):
+        """
+        :param db_path: database file path
+        """
         self.db_path = db_path
+        self.execution_times = []
+        self.retry_count = 0
+        self.artificial_delay = None
 
-    def execute(self, *args, **kwargs):
+    def set_artificial_delay(self, delay: float):
+        self.artificial_delay = delay
+
+    def execute_multiple(self, statements: list[str]) -> sqlite3.Cursor:
         """
         Execute a command on a sqlite3 database, with an auto-commit and a retry mechanism to handle multiple threads/processes.
         """
 
+        start = time.time()
         conn = sqlite3.connect(self.db_path, isolation_level="EXCLUSIVE")
         cursor = conn.cursor()
-        conn.execute("BEGIN EXCLUSIVE TRANSACTION")  # lock the database
 
         count = 0
         new_cursor = None
         while new_cursor is None and count <= MAX_TRIES:
             count += 1
             try:
-                new_cursor = cursor.execute(*args, **kwargs)
+                conn.execute("BEGIN EXCLUSIVE TRANSACTION")  # lock the database
+                for statement in statements:
+                    new_cursor = cursor.execute(statement)
+                if self.artificial_delay is not None:
+                    time.sleep(self.artificial_delay)
                 conn.commit()
             except sqlite3.OperationalError as e:
                 if "database is locked" in str(e):
                     conn.rollback()
                     new_cursor = None
+                    self.retry_count += 1
+                    log.info(f"Database is locked, retrying {count}/{MAX_TRIES}")
                     time.sleep(random.random() * MAX_BACKOFF)
                 else:
                     log.warning(e)
         if new_cursor is None:
             raise MSQLiteMaxRetriesError(f"Database is locked after {count} tries")
+        self.execution_times.append(time.time() - start)
         return new_cursor
+
+    def execute(self, statement: str) -> sqlite3.Cursor:
+        """
+        Execute a command on a sqlite3 database, with an auto-commit and a retry mechanism to handle multiple threads/processes.
+        """
+
+        return self.execute_multiple([statement])
--- test_msqlite/test_msqlite.py
+++ test_msqlite/test_msqlite.py
@@ -1,7 +1,10 @@
 import sqlite3
 from pathlib import Path
+from multiprocessing import Pool
+import time
 
 from msqlite import MSQLite
+from msqlite.msqlite import MAX_BACKOFF
 
 
 def get_temp_dir() -> Path:
@@ -13,11 +16,11 @@ def get_temp_dir() -> Path:
 def test_msqlite_single_thread():
     print(f"{sqlite3.threadsafety=}")
 
-    db_path = Path(get_temp_dir(), "test_msqlite_single_thread.db")
+    db_path = Path(get_temp_dir(), "test_msqlite_db")
     db_path.unlink(missing_ok=True)
     db = MSQLite(db_path)
     db.execute("CREATE TABLE stuff(name, color, year)")
-    db.execute("INSERT INTO stuff VALUES ('table', 'brown', 2020), ('chair', 'black', 2019)")
+    db.execute("INSERT INTO stuff VALUES ('plate', 'brown', 2020), ('chair', 'black', 2019)")
     response = db.execute("SELECT * FROM stuff")
     for row in response:
         print(row)
@@ -25,20 +28,30 @@ def test_msqlite_single_thread():
     response = db.execute("SELECT * FROM stuff")
     for row in response:
         print(row)
+    print(f"{db.retry_count=}")
+    print(f"{db.execution_times=}")
+
+
+mp_db_path = Path(get_temp_dir(), "test_msqlite_multi_process.db")
+
+
+def _access_db(value: int):
+    db = MSQLite(mp_db_path)
+    db.set_artificial_delay(MAX_BACKOFF)  # delay so we'll get some retries
+    db.execute(f"INSERT INTO stuff VALUES ({value}, {time.time()})")
+    if (retry_count := db.retry_count) > 0:
+        print(f"{value=}:{retry_count=}")
+    return db.retry_count
 
 
 def test_msqllite_multi_thread():
     print(f"{sqlite3.threadsafety=}")
 
-    db_path = Path(get_temp_dir(), "test_msqlite_multi_thread.db")
-    db_path.unlink(missing_ok=True)
-    db = MSQLite(db_path)
-    db.execute("CREATE TABLE stuff(name, color, year)")
-    db.execute("INSERT INTO stuff VALUES ('table', 'brown', 2020), ('chair', 'black', 2019)")
-    response = db.execute("SELECT * FROM stuff")
-    for row in response:
-        print(row)
-    db.execute("UPDATE stuff SET color='red' WHERE name='table'")
-    response = db.execute("SELECT * FROM stuff")
-    for row in response:
-        print(row)
+    mp_db_path.unlink(missing_ok=True)
+    db = MSQLite(mp_db_path)
+    db.execute("CREATE TABLE stuff(thing INTEGER PRIMARY KEY, ts NUMERIC)")
+    processes = 1000  # enough so that we'll have at least a few retries
+    with Pool() as pool:
+        results = pool.map(_access_db, range(processes))
+        retries = sum(results)
+        assert retries > 10
